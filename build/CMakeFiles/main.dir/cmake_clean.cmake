7e7a2d23cbd4d8c55fcbff548914f6a0e3498d8f
silver_yin <2099054376@qq.com>
2024-07-02 21:35:39 +0800

    version 0

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "E:/Project/Code/Vscode project/c++/exe/libmain.dll.a"
+  "E:/Project/Code/Vscode project/c++/exe/main.exe"
+  "E:/Project/Code/Vscode project/c++/exe/main.exe.manifest"
+  "E:/Project/Code/Vscode project/c++/exe/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
